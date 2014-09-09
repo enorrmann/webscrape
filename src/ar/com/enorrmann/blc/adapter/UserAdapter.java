395046e240ddc2e93b42841f19d5ba544b612bce
enorrmann <enorrmann@gmail.com>
2014-09-09 09:10:48 -0300

    new links

--- src/ar/com/enorrmann/blc/adapter/UserAdapter.java
+++ src/ar/com/enorrmann/blc/adapter/UserAdapter.java
@@ -11,6 +11,9 @@ public class UserAdapter extends GenericAdapter {
 	public final static String USER_PATH = "/users";
 	final String PAYMENTS_PATH = "/payments";
 	final String RATINGS_PATH = "/ratings";
+	final String RECEIVABLES_PATH = "/receivables";
+	final String PAYABLES_PATH = "/payables";
+	final String CALENDAR_PATH = "/ratings";
 	public final String SITE_USER_PATH = "/user/index/id/";
 
 	public UserAdapter(String url, Document doc, Map<String, Object> adapterMap) {
@@ -33,16 +36,21 @@ public class UserAdapter extends GenericAdapter {
 		investmentsLink.add("href", USER_PATH+"/"+userId+UserInvestmentsAdapter.INVESTMENTS_PATH);
 		linksDto.add(investmentsLink);
 		
-		GenericDTO commentsLink = new GenericDTO();
-		commentsLink.add("rel", "collection/ratings");
-		commentsLink.add("href", USER_PATH+"/"+userId+RATINGS_PATH);
-		linksDto.add(commentsLink);
 		
 		GenericDTO paymentsLink = new GenericDTO();
 		paymentsLink.add("rel", "collection/loans");
 		paymentsLink.add("href", USER_PATH+"/"+userId+LoanAdapter.LOAN_PATH);
 		linksDto.add(paymentsLink);
 		
+		GenericDTO receivablesLink = new GenericDTO();
+		receivablesLink.add("rel", "collection/receivables");
+		receivablesLink.add("href", USER_PATH+"/"+userId+RECEIVABLES_PATH);
+		linksDto.add(receivablesLink);
+		
+		GenericDTO payablesLink = new GenericDTO();
+		payablesLink.add("rel", "collection/receivables");
+		payablesLink.add("href", USER_PATH+"/"+userId+PAYABLES_PATH);
+		linksDto.add(payablesLink);
 		
 		return linksDto;
 		
